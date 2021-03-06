f959a276a218796800ce2eab1e23439d9fc0d225
pradanatech04 <pradanatech04@gmail.com>
2021-03-06 23:30:29 +0700

    kedua

---
+++ main.js
@@ -0,0 +1,22 @@
+var car = {
+    type:"Avanza",
+    power:"1500cc",
+    color:"white",
+
+    start: function(){
+        console.log("Mobil dinyalakan");
+    },
+    drive: function(){
+        console.log("MObil sedang dijalankan");
+    },
+    brake: function(){
+        console.log("Mobil di rem");
+    },
+    stop: function(){
+        console.log("Mobil dimatikan");
+    }
+};
+
+function helo(){
+    console.log("iii");
+}
